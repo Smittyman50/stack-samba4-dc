06c6063cacb7ca66a59e412b6c4daf73aec2e15b
Smittyman50 <149273900+Smittyman50@users.noreply.github.com>
2026-01-01 09:08:04 -0500

    Bump infra-platform to v0.1.1

--- terraform/envs/home/main.tf
+++ terraform/envs/home/main.tf
@@ -1,11 +1,11 @@
 module "vm_fleet" {
   # Preferred: pin to an infra-platform tag
-  # source = "git::http://gitgard.home.arpa:3000/smittyman/infra-platform.git//terraform/modules/proxmox_vm_fleet?ref=v0.1.0"
+  # source = "git::http://gitgard.home.arpa:3000/smittyman/infra-platform.git//terraform/modules/proxmox_vm_fleet?ref=v0.1.1"
 
   # For local development/testing (if you check out both repos next to each other):
   # source = "../../../infra-platform/terraform/modules/proxmox_vm_fleet"
 
-  source = "git::http://gitgard.home.arpa:3000/smittyman/infra-platform.git//terraform/modules/proxmox_vm_fleet?ref=v0.1.0"
+  source = "git::http://gitgard.home.arpa:3000/smittyman/infra-platform.git//terraform/modules/proxmox_vm_fleet?ref=v0.1.1"
 
   vm_template_id   = var.vm_template_id
   vm_template_node = var.vm_template_node
